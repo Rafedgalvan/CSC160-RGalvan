224ec63039d7e6cca3bfbd02db20a78987c3e909
Rafael Galvan <rafedgalv@gmail.com>
2022-02-09 17:50:37 -0700

    Assignment1

---
+++ Assignment1/src/Assignment1.java
@@ -0,0 +1,27 @@
+
+//public class Assignment1 
+
+	//public  void mian(String[] args) {
+		// TODO Auto-generated method stub
+		//	.out.Print("Hello, World!"));
+			
+
+//	}
+
+// this is what the assignment asks to do
+
+
+
+
+
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello, World!");
+			
+
+		}
+	
+	}
